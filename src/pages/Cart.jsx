bc090e78afad79ed3967cfc52849439f49ab3d56
Afolabi Aminat <Afolabijolayemi@gmail.com>
2024-06-05 20:28:40 +0100

    Adjustment of Cart Icons

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,450 +1,86 @@
-import React, { useEffect, useState } from "react";
-import styled from "styled-components";
-import TextInput from "../components/TextInput";
-import Button from "../components/Button";
-import { addToCart, deleteFromCart, getCart, placeOrder } from "../api";
-import { useNavigate } from "react-router-dom";
-import { CircularProgress } from "@mui/material";
-import { useDispatch } from "react-redux";
-import { openSnackbar } from "../redux/reducers/SnackbarSlice";
-import { DeleteOutline } from "@mui/icons-material";
+import React from "react";
 
-const Container = styled.div`
-  padding: 20px 30px;
-  padding-bottom: 200px;
-  height: 100%;
-  overflow-y: scroll;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  gap: 30px;
-  @media (max-width: 768px) {
-    padding: 20px 12px;
-  }
-  background: ${({ theme }) => theme.bg};
-`;
-const Section = styled.div`
-  width: 100%;
-  max-width: 1400px;
-  padding: 32px 16px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  font-size: 22px;
-  gap: 28px;
-`;
-const Title = styled.div`
-  font-size: 28px;
-  font-weight: 500;
-  display: flex;
-  justify-content: ${({ center }) => (center ? "center" : "space-between")};
-  align-items: center;
-`;
-
-const Wrapper = styled.div`
-  display: flex;
-  gap: 32px;
-  width: 100%;
-  padding: 12px;
-  @media (max-width: 750px) {
-    flex-direction: column;
-  }
-`;
-const Left = styled.div`
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  gap: 12px;
-  @media (max-width: 750px) {
-    flex: 1.2;
-  }
-`;
-const Table = styled.div`
-  font-size: 16px;
-  display: flex;
-  align-items: center;
-  gap: 30px;
-  ${({ head }) => head && `margin-bottom: 22px`}
-`;
-const TableItem = styled.div`
-  ${({ flex }) => flex && `flex: 1; `}
-  ${({ bold }) =>
-    bold &&
-    `font-weight: 600; 
-  font-size: 18px;`}
-`;
-const Counter = styled.div`
-  display: flex;
-  gap: 12px;
-  align-items: center;
-  border: 1px solid ${({ theme }) => theme.text_secondary + 40};
-  border-radius: 8px;
-  padding: 4px 12px;
-`;
-
-const Product = styled.div`
-  display: flex;
-  gap: 16px;
-`;
-const Img = styled.img`
-  height: 80px;
-`;
-const Details = styled.div`
-  max-width: 130px;
-  @media (max-width: 700px) {
-    max-width: 60px;
-  }
-`;
-const Protitle = styled.div`
-  color: ${({ theme }) => theme.primary};
-  font-size: 16px;
-  font-weight: 500;
-`;
-const ProDesc = styled.div`
-  font-size: 14px;
-  font-weight: 400;
-  color: ${({ theme }) => theme.text_primary};
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-`;
-const ProSize = styled.div`
-  font-size: 14px;
-  font-weight: 500;
-`;
-
-const Right = styled.div`
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  gap: 12px;
-  @media (max-width: 750px) {
-    flex: 0.8;
-  }
-`;
-const Subtotal = styled.div`
-  font-size: 22px;
-  font-weight: 600;
-  display: flex;
-  justify-content: space-between;
-`;
-const Delivery = styled.div`
-  font-size: 18px;
-  font-weight: 500;
-  display: flex;
-  gap: 6px;
-  flex-direction: column;
-`;
+import CommonSection from "../components/UI/common-section/CommonSection";
+import Helmet from "../components/Helmet/Helmet";
+import "../styles/cart-page.css";
+import { useSelector, useDispatch } from "react-redux";
+import { Container, Row, Col } from "reactstrap";
+import { cartActions } from "../store/shopping-cart/cartSlice";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
-  const navigate = useNavigate();
+  const cartItems = useSelector((state) => state.cart.cartItems);
+  const totalAmount = useSelector((state) => state.cart.totalAmount);
+  return (
+    <Helmet title="Cart">
+      <CommonSection title="Your Cart" />
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12">
+              {cartItems.length === 0 ? (
+                <h5 className="text-center">Your cart is empty</h5>
+              ) : (
+                <table className="table table-bordered">
+                  <thead>
+                    <tr>
+                      <th>Image</th>
+                      <th>Product Title</th>
+                      <th>Price</th>
+                      <th>Quantity</th>
+                      <th>Delete</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {cartItems.map((item) => (
+                      <Tr item={item} key={item.id} />
+                    ))}
+                  </tbody>
+                </table>
+              )}
+
+              <div className="mt-4">
+                <h6>
+                  Subtotal: $
+                  <span className="cart__subtotal">{totalAmount}</span>
+                </h6>
+                <p>Taxes and shipping will calculate at checkout</p>
+                <div className="cart__page-btn">
+                  <button className="addTOCart__btn me-4">
+                    <Link to="/foods">Continue Shopping</Link>
+                  </button>
+                  <button className="addTOCart__btn">
+                    <Link to="/checkout">Proceed to checkout</Link>
+                  </button>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
+
+const Tr = (props) => {
+  const { id, image01, title, price, quantity } = props.item;
   const dispatch = useDispatch();
-  const [loading, setLoading] = useState(false);
-  const [reload, setReload] = useState(false);
-  const [products, setProducts] = useState([]);
-  const [buttonLoad, setButtonLoad] = useState(false);
-  const [deliveryDetails, setDeliveryDetails] = useState({
-    firstName: "",
-    lastName: "",
-    emailAddress: "",
-    phoneNumber: "",
-    completeAddress: "",
-  });
 
-  const getProducts = async () => {
-    setLoading(true);
-    const token = localStorage.getItem("krist-app-token");
-    await getCart(token).then((res) => {
-      setProducts(res.data);
-      setLoading(false);
-    });
-  };
-
-  const calculateSubtotal = () => {
-    return products.reduce(
-      (total, item) => total + item.quantity * item?.product?.price?.org,
-      0
-    );
-  };
-  const convertAddressToString = (addressObj) => {
-    // Convert the address object to a string representation
-    return `${addressObj.firstName} ${addressObj.lastName}, ${addressObj.completeAddress}, ${addressObj.phoneNumber}, ${addressObj.emailAddress}`;
-  };
-
-  const PlaceOrder = async () => {
-    setButtonLoad(true);
-    try {
-      const isDeliveryDetailsFilled =
-        deliveryDetails.firstName &&
-        deliveryDetails.lastName &&
-        deliveryDetails.completeAddress &&
-        deliveryDetails.phoneNumber &&
-        deliveryDetails.emailAddress;
-
-      if (!isDeliveryDetailsFilled) {
-        // Show an error message or handle the situation where delivery details are incomplete
-        dispatch(
-          openSnackbar({
-            message: "Please fill in all required delivery details.",
-            severity: "error",
-          })
-        );
-        return;
-      }
-
-      const token = localStorage.getItem("krist-app-token");
-      const totalAmount = calculateSubtotal().toFixed(2);
-      const orderDetails = {
-        products,
-        address: convertAddressToString(deliveryDetails),
-        totalAmount,
-      };
-
-      await placeOrder(token, orderDetails);
-      dispatch(
-        openSnackbar({
-          message: "Order placed successfully",
-          severity: "success",
-        })
-      );
-      setButtonLoad(false);
-      // Clear the cart and update the UI
-      setReload(!reload);
-    } catch (err) {
-      dispatch(
-        openSnackbar({
-          message: "Failed to place order. Please try again.",
-          severity: "error",
-        })
-      );
-      setButtonLoad(false);
-    }
-  };
-
-  useEffect(() => {
-    getProducts();
-  }, [reload]);
-
-  const addCart = async (id) => {
-    const token = localStorage.getItem("krist-app-token");
-    await addToCart(token, { productId: id, quantity: 1 })
-      .then((res) => {
-        setReload(!reload);
-      })
-      .catch((err) => {
-        setReload(!reload);
-        dispatch(
-          openSnackbar({
-            message: err.message,
-            severity: "error",
-          })
-        );
-      });
-  };
-
-  const removeCart = async (id, quantity, type) => {
-    const token = localStorage.getItem("krist-app-token");
-    let qnt = quantity > 0 ? 1 : null;
-    if (type === "full") qnt = null;
-    await deleteFromCart(token, {
-      productId: id,
-      quantity: qnt,
-    })
-      .then((res) => {
-        setReload(!reload);
-      })
-      .catch((err) => {
-        setReload(!reload);
-        dispatch(
-          openSnackbar({
-            message: err.message,
-            severity: "error",
-          })
-        );
-      });
+  const deleteItem = () => {
+    dispatch(cartActions.deleteItem(id));
   };
   return (
-    <Container>
-      <Section>
-        <Title>Your Shopping Cart</Title>
-        {loading ? (
-          <CircularProgress />
-        ) : (
-          <>
-            {products.length === 0 ? (
-              <>Cart is empty</>
-            ) : (
-              <Wrapper>
-                <Left>
-                  <Table>
-                    <TableItem bold flex>
-                      Product
-                    </TableItem>
-                    <TableItem bold>Price</TableItem>
-                    <TableItem bold>Quantity</TableItem>
-                    <TableItem bold>Subtotal</TableItem>
-                    <TableItem></TableItem>
-                  </Table>
-                  {products.map((item) => (
-                    <Table>
-                      <TableItem flex>
-                        <Product>
-                          <Img src={item?.product?.img} />
-                          <Details>
-                            <Protitle>{item?.product?.name}</Protitle>
-                            <ProDesc>{item?.product?.desc}</ProDesc>
-                          </Details>
-                        </Product>
-                      </TableItem>
-                      <TableItem>${item?.product?.price?.org}</TableItem>
-                      <TableItem>
-                        <Counter>
-                          <div
-                            style={{
-                              cursor: "pointer",
-                              flex: 1,
-                            }}
-                            onClick={() =>
-                              removeCart(item?.product?._id, item?.quantity - 1)
-                            }
-                          >
-                            -
-                          </div>
-                          {item?.quantity}{" "}
-                          <div
-                            style={{
-                              cursor: "pointer",
-                              flex: 1,
-                            }}
-                            onClick={() => addCart(item?.product?._id)}
-                          >
-                            +
-                          </div>
-                        </Counter>
-                      </TableItem>
-                      <TableItem>
-                        {" "}
-                        $
-                        {(item.quantity * item?.product?.price?.org).toFixed(2)}
-                      </TableItem>
-                      <TableItem>
-                        <DeleteOutline
-                          sx={{ color: "red" }}
-                          onClick={() =>
-                            removeCart(
-                              item?.product?._id,
-                              item?.quantity - 1,
-                              "full"
-                            )
-                          }
-                        />
-                      </TableItem>
-                    </Table>
-                  ))}
-                </Left>
-                <Right>
-                  <Subtotal>
-                    Subtotal : ${calculateSubtotal().toFixed(2)}
-                  </Subtotal>
-                  <Delivery>
-                    Delivery Details:
-                    <div>
-                      <div
-                        style={{
-                          display: "flex",
-                          gap: "6px",
-                        }}
-                      >
-                        <TextInput
-                          small
-                          placeholder="First Name"
-                          value={deliveryDetails.firstName}
-                          handelChange={(e) =>
-                            setDeliveryDetails({
-                              ...deliveryDetails,
-                              firstName: e.target.value,
-                            })
-                          }
-                        />
-                        <TextInput
-                          small
-                          placeholder="Last Name"
-                          value={deliveryDetails.lastName}
-                          handelChange={(e) =>
-                            setDeliveryDetails({
-                              ...deliveryDetails,
-                              lastName: e.target.value,
-                            })
-                          }
-                        />
-                      </div>
-                      <TextInput
-                        small
-                        placeholder="Email Address"
-                        value={deliveryDetails.emailAddress}
-                        handelChange={(e) =>
-                          setDeliveryDetails({
-                            ...deliveryDetails,
-                            emailAddress: e.target.value,
-                          })
-                        }
-                      />
-                      <TextInput
-                        small
-                        placeholder="Phone no. +91 XXXXX XXXXX"
-                        value={deliveryDetails.phoneNumber}
-                        handelChange={(e) =>
-                          setDeliveryDetails({
-                            ...deliveryDetails,
-                            phoneNumber: e.target.value,
-                          })
-                        }
-                      />
-                      <TextInput
-                        small
-                        textArea
-                        rows="5"
-                        placeholder="Complete Address (Address, State, Country, Pincode)"
-                        value={deliveryDetails.completeAddress}
-                        handelChange={(e) =>
-                          setDeliveryDetails({
-                            ...deliveryDetails,
-                            completeAddress: e.target.value,
-                          })
-                        }
-                      />
-                    </div>
-                  </Delivery>
-                  <Delivery>
-                    Payment Details:
-                    <div>
-                      <TextInput small placeholder="Card Number" />
-                      <div
-                        style={{
-                          display: "flex",
-                          gap: "6px",
-                        }}
-                      >
-                        <TextInput small placeholder="Expiry Date" />
-                        <TextInput small placeholder="CVV" />
-                      </div>
-                      <TextInput small placeholder="Card Holder name" />
-                    </div>
-                  </Delivery>
-                  <Button
-                    text="Pace Order"
-                    small
-                    onClick={PlaceOrder}
-                    isLoading={buttonLoad}
-                    isDisabled={buttonLoad}
-                  />
-                </Right>
-              </Wrapper>
-            )}
-          </>
-        )}
-      </Section>
-    </Container>
+    <tr>
+      <td className="text-center cart__img-box">
+        <img src={image01} alt="" />
+      </td>
+      <td className="text-center">{title}</td>
+      <td className="text-center">${price}</td>
+      <td className="text-center">{quantity}px</td>
+      <td className="text-center cart__item-del">
+        <i class="ri-delete-bin-line" onClick={deleteItem}></i>
+      </td>
+    </tr>
   );
 };
 
