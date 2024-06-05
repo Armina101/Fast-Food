9b02acd86ad83d2f8e2d7069012c5fd2199ec057
Afolabi Aminat <Afolabijolayemi@gmail.com>
2024-06-05 20:09:25 +0100

    Fixing Errors in App.js

--- src/components/signIn.jsx
+++ src/components/signIn.jsx
@@ -85,7 +85,7 @@ const SignIn = ({ setOpenAuth }) => {
   return (
     <Container>
       <div>
-        <Title>Welcome to Krist 👋</Title>
+        <Title>Welcome to Fast Food 👋</Title>
         <Span>Please login with your details here</Span>
       </div>
       <div style={{ display: "flex", gap: "20px", flexDirection: "column" }}>
--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+import "remixicon/fonts/remixicon.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./index.css";
+
+import store from "./store/store";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { store, persistor } from "./redux/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import { BrowserRouter as Router } from "react-router-dom";
+
+ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
+    <Router>
+      <Provider store={store}>
         <App />
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
+      </Provider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
