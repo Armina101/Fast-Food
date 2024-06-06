44ebf664903eba6b3ff965f5c3e63c7759f12cbc
Afolabi Aminat <Afolabijolayemi@gmail.com>
2024-06-06 21:10:38 +0100

    Fixing Compiling Errors

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import Helmet from "../components/Helmet/Helmet.js";
 import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
 
-import heroImg from "../assets/images/hero.png";
+import heroImg from "../assets/images/hero.jpg";
 import "../styles/hero-section.css";
 
 import { Link } from "react-router-dom";
@@ -13,8 +13,8 @@ import Category from "../components/UI/category/Category.jsx";
 import "../styles/home.css";
 
 import featureImg01 from "../assets/images/service-01.png";
-import featureImg02 from "../assets/images/service-02.png";
-import featureImg03 from "../assets/images/service-03.png";
+import featureImg02 from "../assets/images/service-02.jpg";
+import featureImg03 from "../assets/images/service-03.jpg";
 
 import products from "../assets/fake-data/products.js";
 
@@ -24,9 +24,9 @@ import foodCategoryImg03 from "../assets/images/bread.png";
 
 import ProductCard from "../components/UI/product-card/ProductCard.jsx";
 
-import whyImg from "../assets/images/location.png";
+import whyImg from "../assets/images/location.jpg";
 
-import networkImg from "../assets/images/network.png";
+import networkImg from "../assets/images/network.jpg";
 
 import TestimonialSlider from "../components/UI/slider/TestimonialSlider.jsx";
 
